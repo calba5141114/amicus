b460fb035ad93ab6ade15ccd1bb6fd3bcb830c72
Carlos Alba <soradev4@gmail.com>
2018-03-30 13:30:59 -0700

    firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCS5NWIl9hsIaMiGzO8TsQpwKGm2BobPTM",
+    authDomain: "amicus-c3d0e.firebaseapp.com",
+    databaseURL: "https://amicus-c3d0e.firebaseio.com",
+    projectId: "amicus-c3d0e",
+    storageBucket: "amicus-c3d0e.appspot.com",
+    messagingSenderId: "38748899219"
+  };
+  firebase.initializeApp(config);
